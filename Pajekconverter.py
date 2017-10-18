eafd7604f7cf5434634aa6e44dc49c49138898e2
MatthieudeR <mattderoche@gmail.com>
2017-10-17 21:08:11 -0700

    Basic script for graph format conversion to Pajek - py3 compatibility

--- Pajekconverter.py
+++ Pajekconverter.py
@@ -6,7 +6,7 @@ output_file = "data/9606.protein.links.v10.5.paj"
 
 dico_nodeID = OrderedDict()
 node_id = 0
-print "Generating Nodes list"
+print("Generating Nodes list")
 with open(file_name,'r') as f:
 	f.readline()
 	for line in f:
@@ -22,10 +22,10 @@ with open(file_name,'r') as f:
 
 fout = open(output_file,'w')
 fout.write("*Vertices {}\n".format(len(dico_nodeID)))
-print "Writing nodes"
+print("Writing nodes")
 for node_name,node_id in dico_nodeID.items():
 	fout.write(str(node_id)+" "+node_name+"\n")
-print "Writing edges"
+print("Writing edges")
 fout.write("*arcs\n")
 with open(file_name,'r') as f:
 	f.readline()
