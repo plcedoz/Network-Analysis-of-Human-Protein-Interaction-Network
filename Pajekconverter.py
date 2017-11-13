711662216a08da6a0c3b32dca96a8294e40ec511
vincentbillaut <vct.billaut@gmail.com>
2017-11-12 17:45:05 -0800

    handles compressed data

--- Pajekconverter.py
+++ Pajekconverter.py
@@ -1,15 +1,17 @@
 from collections import OrderedDict
+import gzip
 
-file_name = "data/9606.protein.links.v10.5.txt"
-
+file_name = "data/9606.protein.links.v10.5.txt.gz"
 output_file = "data/9606.protein.links.v10.5.paj"
+
 
 dico_nodeID = OrderedDict()
 node_id = 0
 print("Generating Nodes list")
-with open(file_name,'r') as f:
+with gzip.open(file_name,'rb') as f:
 	f.readline()
 	for line in f:
+		line = line.decode('utf8')
 		start_id,end_id,_ = line.strip().split()
 		if not(dico_nodeID.has_key(start_id)):
 			dico_nodeID[start_id]=node_id
@@ -17,21 +19,22 @@ with open(file_name,'r') as f:
 		if not(dico_nodeID.has_key(end_id)):
 			dico_nodeID[end_id]=node_id
 			node_id+=1
-
-	f.close()
+	f.close()  # with open(...) does it already
 
 fout = open(output_file,'w')
 fout.write("*Vertices {}\n".format(len(dico_nodeID)))
+
 print("Writing nodes")
 for node_name,node_id in dico_nodeID.items():
 	fout.write(str(node_id)+" "+node_name+"\n")
+
 print("Writing edges")
 fout.write("*arcs\n")
-with open(file_name,'r') as f:
+with gzip.open(file_name,'rb') as f:
 	f.readline()
 	for line in f:
+		line=line.decode('utf8')
 		start_id,end_id,weight = line.strip().split()
 		fout.write(" ".join(map(str,[dico_nodeID[start_id],dico_nodeID[end_id],weight]))+"\n")
-
-	f.close()
+	f.close()  # with open(...) does it already
 fout.close()
