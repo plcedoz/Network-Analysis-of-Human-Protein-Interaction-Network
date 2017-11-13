ef2abecdff87ab645cf752a0257efce480a48093
vincentbillaut <vct.billaut@gmail.com>
2017-11-12 19:00:56 -0800

    minor change

--- main_script.py
+++ main_script.py
@@ -5,7 +5,7 @@ import networkx as nx
 from read_graph import read_graph
 
 # Loading PPI graph
-Graph = read_graph()
+Graph, node_names = read_graph()
 print("Loaded graph:\n\t{} nodes\n\t{} edges".format(
     Graph.number_of_nodes(),
     Graph.number_of_edges()
