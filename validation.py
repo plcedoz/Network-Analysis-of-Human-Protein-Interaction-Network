400958c5d9754c6b2a43bfa28e1fe82f5165db1d
plcedoz <plcedoz@stanford.edu>
2017-11-14 10:24:37 -0800

    drugBank dataset

--- validation.py
+++ validation.py
@@ -1,14 +1,20 @@
 import numpy as np
-%matplotlib inline
 import pandas as pd
 import gseapy as gp
-import matplotlib.pyplot as plt
 import math
 from scipy.stats import hypergeom
 
 
 def enrichr_validation(gene_list, gene_rank=None, outdir="validation_results", gene_sets='KEGG_2016'):
+    """
+    Perform the enrichr tool (http://amp.pharm.mssm.edu/Enrichr/)
+    Enrichment of a gene list
     
+    Args:
+        -gene_list (list): Gene list to analyze
+        -gene_rank (list): Ranking of the genes (according to a scoring function)
+    
+    """
     if gene_rank == None:
         enr = gp.enrichr(gene_list=gene_list, description='pathway', gene_sets='KEGG_2016', outdir='test',
                          cutoff=0.05, format='png')
@@ -21,18 +27,29 @@ def enrichr_validation(gene_list, gene_rank=None, outdir="validation_results", g
 
 
 def prerank_validation(gene_list, gene_rank, outdir="validation_results", gene_sets='KEGG_2016'):
+    """
+    Perform the prerank tool (http://software.broadinstitute.org/cancer/software/genepattern/modules/docs/GSEAPreranked/1)
+    Enrichment of a gene list
     
+    Args:
+        -gene_list (list): Gene list to analyze
+        -gene_rank (list): Ranking of the genes (according to a scoring function)
+    
+    """
     rnk = pd.DataFrame(np.array([gene_list, gene_rank]).T, columns = ['gene', 'score'])
     prerank = gp.prerank(rnk=rnk, gene_sets=gene_sets, outdir=outdir,format='png')        
         
     return prerank.res2d
 
 
-def get_cancer_genes(filename="validation_datasets/cancer_gene_census.csv"):
+def get_cancer(filename="validation_datasets/cancer_gene_census.csv"):
+    """
+    Extract a gene list from Cancer Gene Census
 
+    """
     cancer_gene_census = pd.read_csv(filename)
-    gene_symbols = cancer_gene_census.loc[:,"Gene Symbol"]
-    gene_names = cancer_gene_census.loc[:,"Name"]
+    gene_symbols = cancer_gene_census.loc[:,"Gene Symbol"].values.tolist()
+    gene_names = cancer_gene_census.loc[:,"Name"].values.tolist()
     gene_string = []
     list_of_synonyms = cancer_gene_census.loc[:,"Synonyms"]
     for synonyms in list_of_synonyms:
@@ -44,8 +61,11 @@ def get_cancer_genes(filename="validation_datasets/cancer_gene_census.csv"):
     return gene_symbols, gene_names, gene_string
 
 
-def get_mendelian_genes(filename="validation_datasets/mim2gene.txt"):
-    
+def get_mendelian(filename="validation_datasets/mim2gene.txt"):
+    """
+    Extract a gene list from Online Mendelian Inheritance in Man (OMIM) (omim.org)
+
+    """
     gene_entrez = []
     gene_symbols = []
     gene_string = []
@@ -59,6 +79,25 @@ def get_mendelian_genes(filename="validation_datasets/mim2gene.txt"):
                         gene_string.append(line.split('\t')[4])
 
     return gene_symbols, gene_entrez, gene_string
+
+
+
+def get_drugbank(molecule_type="target", subset="all"):
+    """
+    Extract a gene list from Drugbank
+    
+    Args:
+        -molecule_type: "carrier", "enzyme", "target", "transporter"
+        -subset: "all" or "approved"
+        
+    """
+    data = pandas.read_csv("validation_datasets/drugbank_%s_%s_polypeptide_ids.csv/all.csv"%(subset, molecule_type))
+    data = data[data["Species"]=="Human"]
+    gene_symbols = data["Gene Name"].values.tolist()
+    protein_names = data["Name"].values.tolist()
+    uniprot_ID = data["UniProt ID"].values.tolist()
+    
+    return gene_symbols, protein_names, uniprot_ID
 
 
 def compare_gene_lists(query_gene_list, ref_gene_list):
