80dac3d1881c6739e39a12157e74febeba9f0c6c
vincentbillaut <vct.billaut@gmail.com>
2017-11-14 21:56:03 -0800

    changed the output of compare_gene_lists

--- validation.py
+++ validation.py
@@ -8,7 +8,7 @@ from scipy.stats import hypergeom
 def get_query_and_rank(features, node_names, index=0, mapping_file="validation_datasets/entrez_to_ENSP_to_symbols.csv"):
     """
     Converts the features to a gene list with their ranks
-    
+
     Args:
         -features: Array of features.
         -node_names: List of node names.
@@ -23,28 +23,25 @@ def get_query_and_rank(features, node_names, index=0, mapping_file="validation_d
     gene_rank = scores.copy()
     gene_rank.sort()
     gene_rank = gene_rank[::-1].tolist()
-    
+
     mapping = pd.read_csv(mapping_file)
     string_to_symbol = {}
     for i in range(len(mapping)):
         string_to_symbol[str(mapping.iloc[i,2])] = str(mapping.iloc[i,3])
     gene_rank = [gene for i,gene in enumerate(gene_rank) if gene_query[i] in string_to_symbol.keys()]
     gene_query = [string_to_symbol[gene] for gene in gene_query if gene in string_to_symbol.keys()]
-    
+
     return gene_query, gene_rank
 
 
 def compare_gene_lists(gene_query, gene_rank, gene_ref):
-    
     N = len(gene_query)
     n = 100
     M = len(list(set(gene_query) & set(gene_ref)))
     m = len(list(set(gene_query[0:n]) & set(gene_ref)))
-    print(N,n,M,m)
-    
+    print("""#elements in gene_query : \t\t\t{} (cutoff after {})
+#elements from gene_ref in gene_query : \t{} ({}%)
+#elements from gene_ref in gene_query[0:{}] : \t{} ({}%)""".format(N,n,M, int((100*M)/N),n,m, int((100*m)/n)))
     pass
     #hypergeometric test?
     #Proportion of indispensable vs neutral vs dispensables in the ref_gene_list
-    
-    
-    
