cb10468fe2ceacdee45de8cf87f9d6cc26291fa2
vincentbillaut <vct.billaut@gmail.com>
2017-11-16 12:20:12 -0800

    dealing with dump scripts

---
+++ closenessdump.py
@@ -0,0 +1,11 @@
+import networkx as nx
+
+from read_graph import read_graph
+from common.pipeline import Pipeline
+from common.feature_generators import *
+
+# Loading PPI graph
+Graph, node_names = read_graph(directed=False)
+
+pipeline = Pipeline(ClosenessCentrality())
+_ = pipeline.apply(Graph)
