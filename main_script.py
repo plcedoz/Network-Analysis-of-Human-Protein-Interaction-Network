d3412eb89c11ddd6bb74c8b3b6778e648677fe5f
vincentbillaut <vct.billaut@gmail.com>
2017-11-16 17:08:13 -0800

    main_script edited - added compare_feature_distribution

--- main_script.py
+++ main_script.py
@@ -22,7 +22,7 @@ print("Loaded graph:\n\t{} nodes\n\t{} edges".format(
 #########################
 
 # The pipeline object takes as an argument the sequence of features we want
-pipeline = Pipeline(Degree(default_dump=True,default_recomputing=False), ExpectedDegree(default_dump=True,default_recomputing=False))
+pipeline = Pipeline(Degree(default_dump=True,default_recomputing=False), ExpectedDegree(default_dump=True,default_recomputing=False), ClusteringCoefficient())
 features = pipeline.apply(Graph, verbose=True)
 
 
@@ -30,14 +30,29 @@ features = pipeline.apply(Graph, verbose=True)
 # Learning
 #########################
 
-gene_query, gene_rank = get_query_and_rank(features, node_names, index=0)
+gene_query, gene_rank, _ = get_query_and_rank(features, node_names, index=0)
 
 #########################
 # Validation
 #########################
 
 #Perform gene set enrichment analysis (GSEA) on a variety of gene sets directories
-gene_sets_directories = [u'Cancer_Cell_Line_Encyclopedia', u'ChEA_2016', u'DrugMatrix', u'GeneSigDB', u'KEGG_2016', u'LINCS_L1000_Chem_Pert_down', u'LINCS_L1000_Chem_Pert_up', u'MSigDB_Computational', u'MSigDB_Oncogenic_Signatures', u'OMIM_Disease', u'OMIM_Expanded', u'PPI_Hub_Proteins', u'Panther_2016', u'Reactome_2016']
+gene_sets_directories = [
+    u'Cancer_Cell_Line_Encyclopedia',
+    u'ChEA_2016',
+    u'DrugMatrix',
+    u'GeneSigDB',
+    u'KEGG_2016',
+    u'LINCS_L1000_Chem_Pert_down',
+    u'LINCS_L1000_Chem_Pert_up',
+    u'MSigDB_Computational',
+    u'MSigDB_Oncogenic_Signatures',
+    u'OMIM_Disease',
+    u'OMIM_Expanded',
+    u'PPI_Hub_Proteins',
+    u'Panther_2016',
+    u'Reactome_2016'
+    ]
 enrichr = enrichr_validation(gene_query, gene_rank=None, outdir="validation_results", gene_sets='KEGG_2016')
 prerank = prerank_validation(gene_query, gene_rank, outdir="validation_results", gene_sets='KEGG_2016')
 
@@ -46,4 +61,7 @@ prerank = prerank_validation(gene_query, gene_rank, outdir="validation_results",
 gene_ref = get_gene_ref(source="cancer")
 
 compare_gene_lists(gene_query, gene_rank, gene_ref)
-
+for i,feat in enumerate(pipeline.get_generator_names()):
+    print("Comparing reference and whole sample on: [{}]".format(feat))
+    print('\t',compare_feature_distribution(features[,i], gene_ref, feat+'_distribution_comparison.png'))
+print("############\n")
